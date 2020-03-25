bc96b00c6813b149bd78733111b3e03a880e6972
decha-hints <decha.hints@gmail.com>
2020-03-25 22:49:09 +0900

    성장별/컨텐츠별 나눠보기 기능 추가

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@ const postfix = "_content_li";
 const goTo = function(seqno) {
     window.open("https://cafe.naver.com/destinychild/" + seqno);
 }
-const writeData = function() {
-    const sheet = document.createElement('style');
-    const head = document.head || document.getElementsByTagName('head')[0];
+
+let sheet = null;
+const head = document.head || document.getElementsByTagName('head')[0];
+const contentsBody = document.getElementById("contentsBody");
+
+const writeDataByLevel = function(contents) {
+
+    if (sheet != null) head.removeChild(sheet);
+
+    sheet = document.createElement('style');
+
     const classified = Object.keys(contents);
+
+    const contentsBodyElements = [];
 
     for (let i in classified) {
 
@@ -18,18 +28,47 @@ const writeData = function() {
         sheet.appendChild(document.createTextNode(sheet_str));
         head.appendChild(sheet);
 
-        const ulId = keyword + postfix;
-        const ulElement = document.getElementById(ulId);
+        contentsBodyElements.push("<section class='section_", keyword, "'>");
+        contentsBodyElements.push("<div class='title'>");
+        contentsBodyElements.push("<h2 class='", keyword, "'> ", contents[keyword].label, "</h2>");
+        contentsBodyElements.push("</div>");
+        contentsBodyElements.push("<div class='content'>");
+        contentsBodyElements.push("<ul id='", keyword, postfix, "'>");
 
-        const liElements = [];
-        const elements = contents[keyword];
+        const elements = contents[keyword].contents;
         for (let i in elements) {
             const element = elements[i];
             // liElements.push("<li onclick='goTo(" + element.seqno + ")'>[" + element.category + "] " + element.summary + "(" + element.title + " by." + element.author+ ") - " + element.registered + "</li>")
-            liElements.push("<li><span onclick='goTo(" + element.seqno + ")'>[" + categories[element.category].label + "] " + element.title + " - by." + element.author+ " - " + element.registered + "</span></li>")
+            contentsBodyElements.push("<li><span onclick='goTo(" + element.seqno + ")'>[" + categories[element.category].label + "] " + element.title + " - by." + element.author+ " - " + element.registered + "</span></li>")
         }
 
-        ulElement.innerHTML = liElements.join("");
+        contentsBodyElements.push("</ul>");
+        contentsBodyElements.push("</div>");
+        contentsBodyElements.push("</section>");
+
     }
+
+    contentsBody.innerHTML = contentsBodyElements.join('');
+
 }
-writeData();
+writeDataByLevel(contents);
+
+let isFirst = true;
+const writeDataByCategory = function() {
+
+    if (isFirst === true) {
+        isFirst = false;
+        const classified = Object.keys(contents);
+        for (let i in classified) {
+            const keyword = classified[i];
+            const contentsByLevel = contents[keyword].contents;
+            for (let i in contentsByLevel) {
+                const content = contentsByLevel[i];
+                categories[content.category].contents.push(content);
+            }
+        }
+    }
+
+    writeDataByLevel(categories);
+
+}
